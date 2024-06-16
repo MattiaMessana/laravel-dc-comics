914800a61784dd86d75cde8d35548252ba046378
MattiaMessana <mattia.messana@icloud.com>
2024-06-16 11:40:48 +0200

    fix:comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,14 +9,9 @@
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Titolo</th>
-                    {{-- <th scope="col">Descrizione</th> --}}
                     <th scope="col">Poster</th>
                     <th scope="col">Prezzo</th>
-                    <th scope="col">Serie</th>
-                    <th scope="col">Venduto il</th>
-                    <th scope="col">Tipologia</th>
                     <th scope="col">Azioni</th>
-
                 </tr>
             </thead>
             <tbody>
@@ -24,12 +19,8 @@
                     <tr>
                         <th scope="row">{{$comic->id}}</th>
                         <td>{{$comic->title}}</td>
-                        {{-- <td>{{$comic->description}}</td> --}}
-                        <td><img class="w-100" src="{{$comic->thumb}}" alt=""></td>
+                        <td><img class="w-50" src="{{$comic->thumb}}" alt=""></td>
                         <td>{{$comic->price}}</td>
-                        <td>{{$comic->series}}</td>
-                        <td>{{$comic->sale_date}}</td>
-                        <td>{{$comic->type}}</td>
                         <td>
                             <a class="btn btn-primary" href="{{route('comics.show', ['comic' => $comic->id])}}">Dettagli</a>
                         </td>
