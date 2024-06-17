adad4258e1b321483c49602505eb9f1189f6c04b
MattiaMessana <mattia.messana@icloud.com>
2024-06-17 15:22:00 +0200

    add: fix comics.edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -13,40 +13,42 @@
 
             <div class="mt-3 w-100">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+                <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
             </div>
 
             <div class="mt-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+                <textarea class="form-control" name="description" id="description" cols="30" rows="10">
+                    {{ $comic->description }}
+                </textarea>
             </div>
 
             <div class="mt-3">
                 <label for="thumb" class="form-label">Immagine (inserire percorso)</label>
-                <input type="url" class="form-control" id="thumb" name="thumb">
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
             </div>
 
             <div class="mt-3">
                 <label for="price" class="form-label">Prezzo (indicare valuta)</label>
-                <input type="text" class="form-control" id="price" name="price">
+                <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price}}">
             </div>
 
             <div class="mt-3">
                 <label for="series" class="form-label">Serie fumetto</label>
-                <input type="text" class="form-control" name="series" id="series">
+                <input type="text" class="form-control" name="series" id="series" value="{{ $comic->series }}">
             </div>
 
             <div class="mt-3">
                 <label for="sale_date" class="form-label">Data di vendita</label>
-                <input type="date" class="form-control" name="sale_date" id="sale_date">
+                <input type="date" class="form-control" name="sale_date" id="sale_date" value="{{ $comic->sale_date}}">
             </div>
 
             <div class="mt-3">
                 <label for="type" class="form-label">Tipologia</label>
                 <select class="form-select" name="type" id="type">
-                    <option selected>Seleziona</option>
-                    <option value="comic book">Comic Book</option>
-                    <option value="graphic novel">Grahic Novel</option>
+                    <option>Seleziona</option>
+                    <option @selected($comic->type === "comic book") value="comic book">Comic Book</option>
+                    <option @selected($comic->type === "graphic novel") value="graphic novel">Grahic Novel</option>
                 </select>
             </div>
         
