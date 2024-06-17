26b8f54a761ff75e7bec7f2dd5532e00322ebae6
MattiaMessana <mattia.messana@icloud.com>
2024-06-17 14:09:01 +0200

    add: seeder command  in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //mi permette di inserire tutti i seeders tramite un unico comando 
+        $this->call([
+            ComicsTableSeeder::class
+        ]);
     }
 }
